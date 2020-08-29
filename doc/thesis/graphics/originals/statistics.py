845af30bcdf6661825a3670991130e772460099f
Dominik Mueller <me@muellerdominik.ch>
2020-08-29 23:26:22 +0200

    Add theoretical background, training, inference and verification

---
+++ doc/thesis/graphics/originals/statistics.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+'''aionfpga ~ statistics
+Copyright (C) 2020 Dominik Müller and Nico Canzani
+'''
+
+import matplotlib.pyplot as plt
+
+import fhnwtoys.training as fh
+
+# objects_plot = [
+#     'Nerf Dart', 'Football', 'Table Tennis', 'Shuttlecock', 'Sporf', 'Arrow',
+#     'Featherball', 'Floorball', 'Spiky Ball', 'Tesafilm', 'Sponge',
+#     'Red Duplo', 'Green Duplo', 'Duplo Figure', 'Foam Die', 'Infant Shoe',
+#     'Bunny', 'Glove', 'Hemp Cord', 'Paper Ball', 'Beer Cap', 'Water Bottle'
+# ]
+
+objects_plot = fh.objects_ui
+
+def main():
+    noo = fh.num_objects
+
+    frames = [0]*noo
+    frames_partial = [0]*noo
+
+    rows = fh.fetch_all_rows(fh.tab_frames)
+    for row in rows:
+        if row[6] == 1: # valid
+            if row[7] == 0: # not partial
+                frames[fh.objects.index(row[5])] += 1
+            else:
+                frames_partial[fh.objects.index(row[5])] += 1
+
+    frames_valid = sum(frames) + sum(frames_partial)
+    frames_invalid = len(rows) - frames_valid
+
+    print(f'frames_valid {frames_valid}')
+    print(f'frames_invalid {frames_invalid}')
+
+    for idx, o in enumerate(fh.objects):
+        if frames[idx] < 500:
+            print(f'{o}: {frames[idx]} (+{frames_partial[idx]} partial => {frames[idx] + frames_partial[idx]} total)')
+
+    plt.figure(figsize=[12, 6], tight_layout=True)
+    p1 = plt.bar(objects_plot, frames, color=(0, 0.4470, 0.7410))
+    p2 = plt.bar(objects_plot, frames_partial, bottom=frames, color=(0.8500, 0.3250, 0.0980))
+    plt.legend((p1[0], p2[0]), ('Object fully visible', 'Object partially visible'))
+    plt.xticks(rotation=30, ha='right') # formerly 45
+    plt.savefig('statistics.pdf', transparent=True)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ doc/thesis/graphics/originals/weighting_function.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+'''aionfpga ~ weighting function
+Copyright (C) 2020 Dominik Müller and Nico Canzani
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+end = 28
+
+# adapted from https://stackoverflow.com/questions/33737736/matplotlib-axis-arrow-tip
+def arrowed_spines(fig, ax):
+
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    xmax += 1
+    ymax += 0.1
+
+    # spines
+    ax.spines['left'].set_position('zero')
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    # removing the axis ticks
+    ax.grid(False)
+
+    # get width and height of axes object to compute matching arrowhead length and width
+    dps = fig.dpi_scale_trans.inverted()
+    bbox = ax.get_window_extent().transformed(dps)
+    width, height = bbox.width, bbox.height
+
+    # manual arrowhead width and length
+    hw = 1./30.*(ymax-ymin)
+    hl = 1./30.*(xmax-xmin)
+    lw = 1. # axis line width
+    ohg = 0.3 # arrow overhang
+
+    # compute matching arrowhead length and width
+    yhw = hw/(ymax-ymin)*(xmax-xmin)* height/width
+    yhl = hl/(xmax-xmin)*(ymax-ymin)* width/height
+
+    # draw x and y axis
+    ax.arrow(xmin, 0, xmax-xmin, 0., fc='k', ec='k', lw = lw,
+             head_width=hw, head_length=hl, overhang = ohg,
+             length_includes_head= True, clip_on = False)
+
+    ax.arrow(0, ymin, 0., ymax-ymin, fc='k', ec='k', lw = lw,
+             head_width=yhw, head_length=yhl, overhang = ohg,
+             length_includes_head= True, clip_on = False)
+
+def y(k, N=end):
+    return np.sin(k/(N + 1) * np.pi)**2
+
+t1 = np.linspace(-0.5, end + 1.5, 1001)
+y1 = y(t1)
+
+t2 = np.linspace(1, min(22,end), min(22,end))
+y2 = y(t2)
+
+rc = {"xtick.direction" : "inout", "ytick.direction" : "inout", "xtick.major.size" : 6, "ytick.major.size" : 6}
+with plt.rc_context(rc):
+    fig, ax = plt.subplots(figsize=[8, 4])
+    ax.grid(b=True, which='major', color='#666666', linestyle='-')
+    ax.grid(b=True, which='minor', color='#bbbbbb', linestyle='-')
+    ax.set_xticks(list(range(1,end+3,2)))
+    ax.set_yticks([0.2, 0.4, 0.6, 0.8, 1.0])
+    ax.plot(t1, y1, color='gray', linestyle='--')
+    ax.scatter(t2, y2, zorder=3, color=(0, 0.4470, 0.7410))
+
+    for i in range(1, min(22,end) + 1):
+        ax.vlines(i, 0, y2[i-1], color='gray', lw=1, linestyles='dotted')
+
+    plt.xlabel('$k$')
+    plt.ylabel('Weighting Factor $w_k$')
+
+    arrowed_spines(fig, ax)
+
+    plt.savefig('weighting_function.pdf', transparent=True, bbox_inches='tight')
+    plt.show()
